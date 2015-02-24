2c03eff9abb9985f2f79ecec9fd90ef4aa86d010
adanielyan <aadanielyan@gmail.com>
2015-02-24 17:03:43 -0500

    Added regionalData template

--- client/js/app.js
+++ client/js/app.js
@@ -3,14 +3,18 @@ angular
     'lbServices',
     'ui.router'
   ])
-  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
-      $urlRouterProvider) {
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('project', {
         url: '',
         templateUrl: 'views/project.html',
         controller: 'ProjectController'
-      });
+      })
+      .state('regionalData', {
+        url: '/regions/:projectId',
+        templateUrl: 'views/regionalData.html',
+        controller: 'ProjectController'
+      });;
 
     $urlRouterProvider.otherwise('project');
   }]);
--- client/js/controllers/projects.js
+++ client/js/controllers/projects.js
@@ -1,36 +1,56 @@
 angular
-  .module('app')
-  .controller('ProjectController', ['$scope', '$state', 'Project', function($scope,
-      $state, Project) {
-    $scope.projects = [];
-    function getProjects() {
-      Project
-        .find()
-        .$promise
-        .then(function(results) {
-          $scope.projects = results;
-        });
-    }
-    getProjects();
+    .module('app')
+    .controller('ProjectController', ['$scope', '$state', 'Project', function($scope, $state, Project) {
+      $scope.projects = [];
+      $scope.regionalData = [];
+      $scope.project = null;
 
-    $scope.addProject = function() {
-      Project
-        .create($scope.newProject)
-        .$promise
-        .then(function(project) {
-          $scope.newProject = '';
-          $scope.projectForm.Name.$setPristine();
-          $('.focus').focus();
-          getProjects();
-        });
-    };
+      function getProjects() {
+        Project
+            .find()
+            .$promise
+            .then(function(results) {
+              $scope.projects = results;
+            });
+      }
+      getProjects();
 
-    $scope.removeProject = function(item) {
-      Project
-        .deleteById(item)
-        .$promise
-        .then(function() {
-          getProjects();
-        });
-    };
-  }]);
+      $scope.addProject = function() {
+        Project
+            .create($scope.newProject)
+            .$promise
+            .then(function(project) {
+              $scope.newProject = '';
+              $scope.projectForm.Name.$setPristine();
+              $('.focus').focus();
+              getProjects();
+            });
+      };
+
+      $scope.removeProject = function(item) {
+        Project
+            .deleteById(item)
+            .$promise
+            .then(function() {
+              getProjects();
+            });
+      };
+
+      $scope.getProjectById = function(id) {
+        Project
+            .findById(id)
+            .$promise
+            .then(function(result) {
+              $scope.project = result;
+            });
+      }
+
+      $scope.getRegionalDataByProjectId = function(id) {
+        Project
+            .getRegionalDataByProjectId(id)
+            .$promise
+            .then(function(results) {
+              $scope.regionalData = results;
+            });
+      }
+    }]);
