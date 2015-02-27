f0e359022f055235b92851e44d9fb0fc0fddb7f9
Armen <aadanielyan@gmail.com>
2015-02-27 02:11:04 -0500

    Added creation dates and owner to Project

--- common/models/project.js
+++ common/models/project.js
@@ -1,3 +1,17 @@
-module.exports = function(Project) {
+var loopback = require('loopback');
 
+module.exports = function(Project) {
+    Project.observe('before save', function updateTimestamp(ctx, next) {
+        if (ctx.instance) {
+            ctx.instance.created = new Date();
+
+            var token = loopback.getCurrentContext().get('accessToken');
+            if(token !== undefined) {
+                ctx.instance.ownerId = token.userId;
+            }
+        } else {
+            ctx.data.modified = new Date();
+        }
+        next();
+    });
 };
